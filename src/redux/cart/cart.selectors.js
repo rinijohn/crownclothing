24e171c1fec465831e7717fb7d18becba52f35ea
rinijohn <rinijohn97@gmail.com>
2021-07-13 20:40:56 +0530

    updating app with reselect library and creating selectors , unwanted preventing redenring of components

---
+++ src/redux/cart/cart.selectors.js
@@ -0,0 +1,17 @@
+import { createSelector } from "reselect";
+
+const selectCart = state => state.cart;         //input selector
+
+export const selectCartItems = createSelector(   //output selector - takes two arguments
+    [selectCart],       //An array of input selectors
+    cart => cart.cartItems      //A function that will return a required value from the input selector/selectors                
+)
+
+export const selectCartItemsCount = createSelector( //an output selctor that uses another output selector
+    [selectCartItems],
+    cartItems => cartItems.reduce(
+        (accumulatedCountValue, cartItem) =>
+        accumulatedCountValue + cartItem.quantity,
+        0
+        )
+)
