4812813218d5e6d680457729e1f22c1ea9c52226
rinijohn <rinijohn97@gmail.com>
2021-07-14 17:03:42 +0530

    added addItem and removeItem fuctionality to checkout item component

--- src/components/checkout-item/checkout-item-component.jsx
+++ src/components/checkout-item/checkout-item-component.jsx
@@ -1,16 +1,33 @@
 import React from 'react';
 import './checkout-item.styles.scss';
+import { connect } from 'react-redux';
+import { addItem, clearItemFromCart, removeItem } from '../../redux/cart/cart.actions';
 
-const ChekoutItem = ({ cartItem:{ name, price, quantity, imageUrl} })=>(
+const ChekoutItem = ({ cartItem, clearItem, addItem, removeItem})=>{
+
+    const { name, price, quantity, imageUrl } = cartItem;      //Destructuring cartItem in fuction call will not give access to cartItem as a whole,
+                                                               //and the cartIem as a whole is needed for clearItemFromCart function, hence its destructed within the function.
+    return(
     <div className='checkout-item'>
         <div className='image-container'>
-            <img src={imageUrl}alt='item-image'/>
+            <img src={imageUrl} alt='item'/>
         </div>
         <span className='name'>{name}</span>
-        <span className='quantity'>{quantity}</span>
+        <span className='quantity'>
+            <div className='arrow' onClick={()=> removeItem(cartItem)}>&#10094;</div>
+            <span className='value'>{quantity}</span>
+            <div className='arrow' onClick={()=> addItem(cartItem)}>&#10095;</div>
+        </span>
         <span className='price'>{price}</span>
-        <div className='remove-button'>&#10007;</div>
+        <div className='remove-button' onClick={()=> clearItem(cartItem)}>&#10007;</div>
     </div>
-)
+    )
+}
 
-export default ChekoutItem;
+const mapDispatchToProps = dispatch =>({
+    clearItem: item => dispatch(clearItemFromCart(item)),
+    addItem: item => dispatch(addItem(item)),
+    removeItem: item => dispatch(removeItem(item))
+})
+
+export default connect(null, mapDispatchToProps)(ChekoutItem);
--- src/redux/cart/cart.utils.js
+++ src/redux/cart/cart.utils.js
@@ -12,3 +12,20 @@ export const addItemToCart = (cartItems, cartItemToAdd) =>{
     return [...cartItems, {...cartItemToAdd, quantity: 1}]
 
 }
+
+export const removeItemFromCart = (cartItems, cartItemToRemove) => {
+    const existingCartItem = cartItems.find(
+        cartItem => cartItem.id === cartItemToRemove.id
+    )
+
+    if(existingCartItem.quantity === 1){
+        return cartItems.filter( cartItem => cartItem.id !== cartItemToRemove.id)
+    }
+
+    return cartItems.map(cartItem =>    //If quantity is greater than 1
+        cartItem.id === cartItemToRemove.id ?
+        {...cartItem, quantity: cartItem.quantity - 1} : //Remove matching item from list
+        cartItem        //Retain undeleted items
+        )
+
+}
